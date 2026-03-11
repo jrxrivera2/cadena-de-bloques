4c049027a3804d927459870b5180545a7465d65b
jrxrivera2 <jalarcon@unimayor.edu.co>
2026-03-11 12:46:08 -0500

    Se cambia puerto para compatibilidad con Render

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const { BlockChain } = require('./cadenadebloques');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
